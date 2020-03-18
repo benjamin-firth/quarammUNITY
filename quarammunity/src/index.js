8551ea01085295ef01c56f93108f27cfa449d2cd
Ben Firth <bfirth93@gmail.com>
2020-03-18 17:14:50 -0600

    Finalize setup

--- quarammunity/src/index.js
+++ quarammunity/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './reducers';
-import './index.css';
-import App from './App';
+import './index.scss';
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(rootReducer, composeWithDevTools());
