598fa857ad126b9841e5adbf5e3cf8ab2d4df52a
Ben Firth <bfirth93@gmail.com>
2020-03-19 12:15:25 -0600

    Add styling for needs card

---
+++ quarammunity/src/components/NeedsCard/NeedsCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NeedsCard.scss';
+
+const NeedsCard = () => {
+  return (
+    <article>
+      <h3>Name: Ben Firth</h3>
+      <h3>Need: Toilet Paper</h3>
+      <h4>Street Address: 2201 Table Dr.</h4>
+      <button type='button'>Click for further information</button>
+    </article>
+  );
+};
+
+export default NeedsCard;
