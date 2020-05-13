28209136d9da9921d1bed9b2307d569d2e81f4ee
Ben Firth <bfirth93@gmail.com>
2020-05-13 11:36:34 -0600

    Add needsReducer to rootReducer

--- quarammunity/src/reducers/index.js
+++ quarammunity/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { loginReducer } from './loginReducer';
+import { needsReducer } from './needsReducer';
 
 export const rootReducer = combineReducers({
-  currentUser: loginReducer
+  currentUser: loginReducer,
+  currentNeeds: needsReducer
 });
